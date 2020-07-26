45f56328a92362673d762beffd3646ce7b5eda49
a-thread <aiden.threadgoode@gmail.com>
2020-07-26 14:31:01 -0400

    updating js

--- assets/script.js
+++ assets/script.js
@@ -33,20 +33,20 @@ $(document).ready(function () {
 
   setInterval(hourUpdate, 6000); // this will update the hour class every minute
 
-//   // Storage for each textArea
-//   $("#hour-9 .description").val(localStorage.getItem("hour-9"));
-//   $("#hour-10 .description").val(localStorage.getItem("hour-10"));
-//   $("#hour-11 .description").val(localStorage.getItem("hour-11"));
-//   $("#hour-12 .description").val(localStorage.getItem("hour-12"));
-//   $("#hour-13 .description").val(localStorage.getItem("hour-13"));
-//   $("#hour-14 .description").val(localStorage.getItem("hour-14"));
-//   $("#hour-15 .description").val(localStorage.getItem("hour-15"));
-//   $("#hour-16 .description").val(localStorage.getItem("hour-16"));
-//   $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+  // Storage for each textArea
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
-//   // Clear Button --> clears local storage & reloads calendar
-//   $("#clearDay").on("click", function () {
-//     localStorage.clear();
-//     location.reload(true);
-//   });
-// });
+  // Clear Button --> clears local storage & reloads calendar
+  $("#clearDay").on("click", function () {
+    localStorage.clear();
+    location.reload(true);
+  });
+});
